38859f1a7dc81a22e47e86d723d712a93ac0b8ef
Shibou <99544992+ShibouDev@users.noreply.github.com>
2022-10-05 12:28:26 +0500

    add glitch text

--- src/components/assets/block/Text/Text.jsx
+++ src/components/assets/block/Text/Text.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import styles from "./text.module.scss"
+import GlitchText from "react-glitch-effect/core/GlitchText"
+
 export const TitleUp = (props) => {
     return (
+        <GlitchText
+          color1={"#FB03F5"}
+          color2="#AA9CFF"
+          duration="5000"
+          >
         <div className={styles.titleUp}>{props.text}</div>
+        </GlitchText>
     )
 }
 export const TitleDown = (props) => {
     return (
+        <GlitchText
+          color1={"#FB03F5"}
+          color2="#AA9CFF"
+          duration="5000">
         <div className={styles.titleDown}>{props.text}</div>
+        </GlitchText>
     )
 }
 export const Desc = (props) => {
