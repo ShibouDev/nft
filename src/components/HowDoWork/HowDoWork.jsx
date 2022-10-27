0cc2704a0ee440cfe5a4500945bb0b600b28173a
ShibouDev <sadandcryalone@gmail.com>
2022-10-27 10:27:31 +0300

    add slider3d

--- src/components/HowDoWork/HowDoWork.jsx
+++ src/components/HowDoWork/HowDoWork.jsx
@@ -3,6 +3,23 @@ import Fade from 'react-reveal/Fade'
 import { TitleUp, Desc } from "../assets/block/Text/Text"
 import playlistdata from '../assets/data/playlist'
 const HowDoWork = () => {
+    const carousel = document.getElementById("carousel")
+    const cellCount = 9
+    let selectedIndex = 0
+
+    const rotateCarousel = () => {
+        const angel = selectedIndex / cellCount * -360
+        carousel.style.transform = 'translateZ(-288) rotateY(' + angel + 'deg)'
+    }
+    const handleClickBack = () => {
+        selectedIndex--;
+        rotateCarousel();
+    }
+
+    const handleClickUp = () => {
+        selectedIndex++;
+        rotateCarousel();
+    }
     return (
         <div className={styles.howdowork}>
             <div className={styles.howdowork_info}>
@@ -10,6 +27,13 @@ const HowDoWork = () => {
                     <TitleUp text="How do Music NFTs work?" />
                     <Desc text="NFTs are unique assets stored and tradable on a blockchain. They are, immutable, scarce and publicly verifiable. In the music world, artistes get to sell limited digital content to their fans who pay via crypto. You can now produce any music content that cannot be counterfeited." />
                 </Fade>
+            </div>
+            <div className={styles.howdowork_scene}>
+                <div className={styles.howdowork_scene_carousel} id="carousel">
+                    {playlistdata.map((el) => (
+                        <img className={styles.howdowork_scene_carousel__cell} src={el.image} alt=""></img>
+                    ))}
+                </div>
             </div>
         </div>
     )
--- src/components/assets/data/playlist.js
+++ src/components/assets/data/playlist.js
@@ -1,4 +1,5 @@
 import heart_sol from '../image/playlist/heart_sol.jpg'
+import post_punk from '../image/playlist/post_punk.jpg'
 const playlistdata = [
     {
         image: heart_sol,
@@ -7,5 +8,62 @@ const playlistdata = [
         tokens: 16,
         genre: 'Pop',
     },
+    {
+        image: post_punk,
+        name: 'Post Punk',
+        price: 0.3234,
+        tokens: 16,
+        genre: 'Punk',
+    },
+    {
+        image: post_punk,
+        name: 'Post Punk',
+        price: 0.3234,
+        tokens: 16,
+        genre: 'Punk',
+    },
+    {
+        image: post_punk,
+        name: 'Post Punk',
+        price: 0.3234,
+        tokens: 16,
+        genre: 'Punk',
+    },
+    {
+        image: post_punk,
+        name: 'Post Punk',
+        price: 0.3234,
+        tokens: 16,
+        genre: 'Punk',
+    },
+    {
+        image: post_punk,
+        name: 'Post Punk',
+        price: 0.3234,
+        tokens: 16,
+        genre: 'Punk',
+    },
+    {
+        image: post_punk,
+        name: 'Post Punk',
+        price: 0.3234,
+        tokens: 16,
+        genre: 'Punk',
+    },
+    {
+        image: post_punk,
+        name: 'Post Punk',
+        price: 0.3234,
+        tokens: 16,
+        genre: 'Punk',
+    },
+    {
+        image: post_punk,
+        name: 'Post Punk',
+        price: 0.3234,
+        tokens: 16,
+        genre: 'Punk',
+    },
+   
 ]
 export default [...playlistdata]
